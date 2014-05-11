b3f0e195e44187907d355bacdbae16e7a207c92f
Maciekek <maciekek@gmail.com>
2014-05-11 19:19:57 +0200

    fix toggle switch and roaming setting save

--- MeteoICM/bin/Debug/AppX/pages/settings/legend/legend.js
+++ MeteoICM/bin/Debug/AppX/pages/settings/legend/legend.js
@@ -4,26 +4,27 @@
     WinJS.Binding.optimizeBindingReferences = true;
     var app = WinJS.Application;
     var ui = WinJS.UI;
-
+    var applicationData = Windows.Storage.ApplicationData.current;
+    var roamingSettings = applicationData.roamingSettings;
 
     ui.Pages.define("/pages/settings/legend/legend.html", {
         ready: function (element, options) {
 
             var toggle = document.getElementById("toggle").winControl;
             toggle.addEventListener("change", this.toggleHandler);
-          
+            toggle.checked = roamingSettings.values["legenda"];
+            
 
         },
         unload: function () {
 
         },
 
-        toggleHandler: function (element) {
+        toggleHandler: function () {
             var toggle = document.getElementById("toggle").winControl;
             console.log(toggle.checked);
             console.log("zmieniłem");
-            var applicationData = Windows.Storage.ApplicationData.current;
-            var roamingSettings = applicationData.roamingSettings;
+            
             roamingSettings.values["legenda"] = toggle.checked;
         }
         
